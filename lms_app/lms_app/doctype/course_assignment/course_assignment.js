9807318467ac609c2b0610eabfbc304b6154b490
abbeyanon <mbitheabigail20@gmail.com>
2025-06-25 22:45:30 +0300

    Added LMS Analytics Dashboard Doctype

---
+++ lms_app/lms_app/doctype/course_assignment/course_assignment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Abigael Lemba and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Course Assignment", {
+// 	refresh(frm) {
+
+// 	},
+// });
